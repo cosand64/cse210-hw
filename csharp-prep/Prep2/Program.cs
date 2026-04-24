03c3608047e01e5f81f9b04572e5efe2b8bfe543
cosand64 <cosand64@gmail.com>
2026-04-23 19:29:26 -0600

    Finished the program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,9 +7,37 @@ class Program
         Console.Write("What is the percent that you recieved? ");
         string userInput = Console.ReadLine();
         int gradePercent = int.Parse(userInput);
+        string letterGrade = "";
         
-        Console.Write($"Your grade is {gradePercent}");
-        
+        if (gradePercent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("You have passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed! You will have to try again!");
+        }
     }
     
 }
