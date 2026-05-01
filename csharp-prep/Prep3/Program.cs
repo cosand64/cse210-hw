ade74551001774e15fcb2f0e50607f7a45eb47d6
cosand64 <cosand64@gmail.com>
2026-04-30 22:35:51 -0600

    prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+        
+        int usersGuess = 0;
+
+        while (usersGuess != randomNumber)
+        {
+            Console.Write("What is your guess at the magic number? ");
+            usersGuess = int.Parse(Console.ReadLine());
+
+            if (usersGuess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (usersGuess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("You Guessed it!");
+
     }
 }
