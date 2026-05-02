c11b32e15a1c1c917d2b35c76dab86554ea54d9b
cosand64 <cosand64@gmail.com>
2026-05-01 19:54:34 -0600

    added first two funcs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptFavoriteNumber()
+    {
+        Console.Write("What is your favorite whole number? ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        return favoriteNumber;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptFavoriteNumber();
     }
 }
