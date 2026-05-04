210b3d3e3a7baad7904a3d14d6a858fdc258b8aa
cosand64 <cosand64@gmail.com>
2026-05-04 14:58:18 -0600

    made cylinder

--- sandbox/CircleApplication/Circle.cs
+++ sandbox/CircleApplication/Circle.cs
@@ -1,6 +1,6 @@
 class Circle
 {
-    double _radius;
+    public double _radius;
 
     public void SetRadius(double radius)
     {
@@ -10,5 +10,15 @@ class Circle
     public double GetArea()
     {
         return Math.PI * _radius * _radius;
+    }
+
+    public double GetCircumference()
+    {
+        return 2 * Math.PI * _radius;
+    }
+
+    public double GetDiameter()
+    {
+        return _radius * 2;
     }
 }
--- sandbox/CircleApplication/Program.cs
+++ sandbox/CircleApplication/Program.cs
@@ -5,5 +5,9 @@
         Circle myCircle = new Circle();
         myCircle.SetRadius(10);
         Console.WriteLine(myCircle.GetArea());
+
+        Circle myCircle2 = new Circle();
+        myCircle2._radius = 30;
+        Console.WriteLine(myCircle2._radius);
     }
 }
