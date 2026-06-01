eb28a78f1ad244a9bdb91f557bdc55efb564e79a
cosand64 <cosand64@gmail.com>
2026-06-01 14:57:20 -0600

    practiced inheritence with the person class

---
+++ sandbox/Person/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person myPerson = new Person("bob", "stuart", 18, 198);
+        Console.WriteLine(myPerson.GetPersonInformation());
+
+        Police myPoliceMan = new Police("Gun", "Robert", "bob", 50, 170);
+
+        Console.WriteLine(myPoliceMan.GetPoliceManInformation());
+    }
+}
