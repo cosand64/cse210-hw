ef77d5771164c5bda675a4b48a9662ce311e3009
cosand64 <cosand64@gmail.com>
2026-06-01 17:37:50 -0600

    completed the weekly prep assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new Assignment("Bobby Lee", "Math");
+        Console.WriteLine(myAssignment.GetSummary());
+
+        MathAssignment myMathAssignment = new MathAssignment("Bobby Lee", "Math", "7e", "1-1000");
+        Console.WriteLine(myMathAssignment.GetSummary());
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+
+        WritingAssignment myWritingAssignment = new WritingAssignment("Sean Spencer", "English 101", "How to trick an entire police department into thinking you're a psycic. ");
+        Console.WriteLine(myWritingAssignment.GetSummary());
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
     }
 }
