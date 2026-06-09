14fb5ff9626bff085c3c6950085b62316d774ca3
cosand64 <cosand64@gmail.com>
2026-06-08 19:07:46 -0600

    animation practice

---
+++ sandbox/Animations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+        int sleepTime = 25;
+
+        // for (int i = 0; i < 20; i++)
+        // {
+        //     Console.Write("+");
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        //     Console.Write("-");
+        //     Thread.Sleep(sleepTime);
+        //     Console.Write("\b");
+        // }
+
+        string animationString = "\\-/|";
+        for (int i = 0; i < 200; i++)
+        {
+            Console.Write(animationString[i % animationString.Length]);
+            Thread.Sleep(sleepTime);
+            Console.Write("\b");
+            // Console.Write("-");
+            // Thread.Sleep(sleepTime);
+            // Console.Write("\b");
+        }
+
+        Console.CursorVisible = true;
+    }
+}
